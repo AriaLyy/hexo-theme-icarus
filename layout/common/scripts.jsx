06bd34e48019b0f3a2cc9ec5084d5f9b2b6f4327
lyy <511455842@QQ.com>
2022-12-31 23:55:04 +0800

    增加线条动画

--- layout/common/scripts.jsx
+++ layout/common/scripts.jsx
@@ -40,6 +40,7 @@ module.exports = class extends Component {
             <script src={url_for('/js/main.js')} defer></script>
             <script src={url_for('/js/night.js')} defer={true}></script>
             <script src={url_for('/js/universe.js')} defer={true}></script>
+            <script src={url_for('https://cdn.bootcss.com/canvas-nest.js/1.0.0/canvas-nest.min.js')} defer={true}></script>
         </Fragment>;
     }
 };
